87192e38841a9ec6d6a127fa344141fbb0d41cb5
alexandru233847 <alexutualexino33@gmail.com>
2024-10-22 17:53:10 +0200

    Car instance

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Car class
 class Car {
-    constructor (brand, year, color, doors, fuel) {
+    constructor(brand, year, color, doors, fuel) {
         this.brand = brand;
         this.year = year;
         this.color = color;
@@ -7,6 +8,21 @@ class Car {
         this.fuel = fuel;
     }
 
-    
+    // Car information
+    information() {
+        return `Brand: ${this.brand}, Year: ${this.year}, Color: ${this.color}, Doors: ${this.doors}, Fuel: ${this.fuel}`;
+    }
+
+    // Car age
+    age() {
+        const year = new Date().getFullYear();
+        return year - this.year;
+    }
 }
+
+// Car instance
+const myCar = new Car("Fiat", "2019", "blue", 4, "gasoline");
+
+console.log (myCar.information());
+console.log (`Car age: ${myCar.age()} years`);
 
