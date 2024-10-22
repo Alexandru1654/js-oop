92262c43e8fdbaae687c0627ffcf130be881a358
alexandru233847 <alexutualexino33@gmail.com>
2024-10-22 17:13:08 +0200

    New instance

---
+++ script.js
@@ -0,0 +1,12 @@
+class Car {
+    constructor (brand, year, color, doors, fuel) {
+        this.brand = brand;
+        this.year = year;
+        this.color = color;
+        this.doors = doors;
+        this.fuel = fuel;
+    }
+
+    
+}
+
